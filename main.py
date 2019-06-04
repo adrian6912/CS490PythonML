e0548cf9edcbf269ae3acb9cdac0a15c616bfe40
adrian6912 <adrian6912@gmail.com>
2019-06-04 11:59:54 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 if __name__ == "__main__":
+    def delCharacter(string, char):
+        """Deletes a specified character from a given string"""
+        ind = string.find(char)
+        if ind == -1: #Character isn't in the original string, return original string
+            return string
+        newString = string[:ind] + string[ind + 1:]
+        return newString
 
-    def replacer(string):
+    def play():
         """Program 2a from ICP1"""
+        string = input("Please input the word Python\n")
+        newString = delCharacter(string, 'n')
+        newString = delCharacter(newString, 'h')
+        reversedString = newString[::-1]
+        return reversedString
+
+    def arith():
+        """Program 2b from ICP1"""
+        x = int(input("Please input a number\n"))
+        y = int(input("Please input another number\n"))
+        return x + y
+
+    def replacer():
+        """Program 3 from ICP1"""
+        string = input("Please input a sentence that contains the word 'python'\n")
         listy = string.split()
         newList = []
         for elmt in listy:
@@ -14,30 +36,6 @@ if __name__ == "__main__":
             newString = newString + " " + elmt
         return newString.strip()
 
-    def delCharacter(string, char):
-        """Deletes a specified character from a given string"""
-        ind = string.find(char)
-        if ind == -1: #Character isn't in the original string, return original string
-            return string
-        newString = string[:ind] + string[ind + 1:]
-        return newString
-
-    def play():
-        """Program 3 from ICP 1"""
-        string = input("Please input the word Python")
-        newString = delCharacter(string, 'n')
-        newString = delCharacter(newString, 'h')
-        reversedString = newString[::-1]
-        print(reversedString)
-
-    def arith():
-        """Program 2b from ICP1"""
-        x = int(input())
-        y = int(input())
-        print(x+y)
-
-    play()
-    arith()
-    x = input()
-    print(replacer(x))
-
+    print(play())
+    print(arith())
+    print(replacer())
