05fdaf6f19eddd045656f0eef796bb0e7f9f96cb
adrian6912 <adrian6912@gmail.com>
2019-06-06 12:01:04 -0500

    Add files via upload

--- ICP2/main.py
+++ ICP2/main.py
@@ -1,16 +1,18 @@
 if __name__=="__main__":
     def lbs_to_kg():
         """ICP part 1 converts a list of pounds to kilograms"""
-        pounds = [150, 155, 145, 148]
-        kilograms = [x * 0.45359237 for x in pounds]
+        weights_in = input("Give me the weight in pounds!\n")
+        pounds = weights_in.split(',')
+        kilograms = [int(x) * 0.45359237 for x in pounds]
         return kilograms
 
     def string_alternative():
         """ICP part 2 returns every other letter of a string"""
-        string = "Good evening"
+        string = input("Input a sentence\n")
         return string[::2]
 
     def word_counter():
+        import string as s
         """ICP part 3 counts the occurrences of individual words in a document"""
         counter = {}
         file = open('ICP2_Part3.txt', 'r+')
@@ -19,10 +21,11 @@ if __name__=="__main__":
         for line in lines:
             words = line.split()
             for word in words:
+                stripped_word = word.strip(s.punctuation)
                 try:
-                    counter[word] = counter[word] + 1
+                    counter[stripped_word.lower()] = counter[stripped_word.lower()] + 1
                 except KeyError:
-                    counter[word] = 1
+                    counter[stripped_word.lower()] = 1
         for word in counter.keys():
             file.write(word + ": " + str(counter[word]) + '\n')
         file.close()
