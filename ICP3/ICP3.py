a05748c6764e73c3e07a7f648cca2bef04133dd6
adrian6912 <adrian6912@gmail.com>
2019-06-11 11:43:31 -0500

    Add files via upload

--- ICP3/ICP3.py
+++ ICP3/ICP3.py
@@ -1,32 +1,51 @@
-# Part 1 of ICP3
+# ICP3
 
 import numpy as np
 import requests as r
 from bs4 import BeautifulSoup
 
+# Part 1 of ICP3
+
 
 class Employee(object):
     """Part 1 of ICP3"""
+    employee_count = 0
+    total_salary = 0
 
     def __init__(self, name=None, family=None, salary=None, department=None):
         self.name = name
         self.family = family
         self.salary = salary
         self.department = department
+        Employee.employee_count += 1
+        Employee.total_salary += salary
+
+    def avg_salary():
+        return Employee.total_salary / Employee.employee_count
+
+    def get_name(self):
+        return self.name
+
+    def get_family(self):
+        return self.family
+
+    def get_salary(self):
+        return self.salary
+
+    def get_department(self):
+        return self.department
+
 
 class FulltimeEmployee(Employee):
     pass
 
-def avg_salary(employee_list):
-    salary_total = 0
-    for employee in employee_list:
-        salary_total += employee.salary
-    return salary_total / len(employee_list)
 
+one = Employee('John Smith', 0, 50000, 'Accounting')
+two = FulltimeEmployee('Jane Smith', 3, 80000, 'IT')
+three = Employee('James Howard', 3, 26000, 'Housekeeping')
 
-xy = Employee('John Smith', 0, 50000, 'Accounting')
-xx = FulltimeEmployee('Jane Smith', 3, 80000, 'IT')
-employees = []
+print("Employee names: ", one.get_name(), two.get_name(), three.get_name())
+print("Average salary: ", str(Employee.avg_salary()))
 
 
 # Part 2 of ICP3
