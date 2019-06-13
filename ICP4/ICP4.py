dd53f9508e684dcdd360ddb96db18b6693318ce6
adrian6912 <adrian6912@gmail.com>
2019-06-13 12:10:28 -0500

    Update ICP4.py

--- ICP4/ICP4.py
+++ ICP4/ICP4.py
@@ -6,14 +6,25 @@ from sklearn.svm import SVC
 from sklearn import metrics
 import pandas as pd
 
+# Load data 
+
 raw_data = pd.read_csv('glass.csv', dtype=float)
+
+# Set the target data, remove it from raw_data
+
 y = raw_data.Type
 raw_data.pop('Type')
+
+# Set the data for the features
 
 columns = raw_data.columns.values
 features = raw_data.loc[:, columns]
 
+# Split features and y into training and test data sets
+
 X_train, X_test, y_train, y_test = tts(features, y, test_size=.3)
+
+# GNB model
 
 gnb = GaussianNB()
 gnb.fit(X_train, y_train)
@@ -22,7 +33,7 @@ y_pred = gnb.predict(X_test)
 print("Accuracy of GNB:", metrics.accuracy_score(y_test, y_pred))
 
 # Part 3 of ICP4
-# Using kernel = 'linear'
+# As kernel='rbf' is the default when a kernel isn't specified, I will use kernel = 'poly'
 
 svc = SVC(kernel='poly', gamma='auto')
 svc.fit(X_train, y_train)
@@ -31,7 +42,6 @@ y_pred = svc.predict(X_test)
 print("Accuracy of SVM with Poly kernel:", metrics.accuracy_score(y_test, y_pred))
 
 # Part 4 of ICP4
-# The SVM automatically uses the RBF kernel when none is provided, so this model is identical to the SVM model above
 
 svc = SVC(kernel='rbf', gamma='auto')
 svc.fit(X_train, y_train)
