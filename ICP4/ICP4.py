181067d91c43d9e4504c7b31390d6fb14db8634c
adrian6912 <adrian6912@gmail.com>
2019-06-13 12:04:33 -0500

    Add files via upload

--- ICP4/ICP4.py
+++ ICP4/ICP4.py
@@ -1,22 +1,41 @@
 # Part 2 of ICP4
 
-from sklearn import preprocessing as p
 from sklearn.model_selection import train_test_split as tts
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 from sklearn import metrics
-import numpy as np
 import pandas as pd
 
-raw_data = pd.read_csv('glass.csv')
-y = raw_data.pop('Type').values
-features = [x for x in raw_data.iterrows()]
+raw_data = pd.read_csv('glass.csv', dtype=float)
+y = raw_data.Type
+raw_data.pop('Type')
 
-X_train, X_test, y_train, y_test = tts(features, y, test_size=.1, random_state=69)
+columns = raw_data.columns.values
+features = raw_data.loc[:, columns]
 
-model = GaussianNB()
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+X_train, X_test, y_train, y_test = tts(features, y, test_size=.3)
 
-print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+y_pred = gnb.predict(X_test)
 
-###Errors are still on this one.
+print("Accuracy of GNB:", metrics.accuracy_score(y_test, y_pred))
+
+# Part 3 of ICP4
+# Using kernel = 'linear'
+
+svc = SVC(kernel='poly', gamma='auto')
+svc.fit(X_train, y_train)
+y_pred = svc.predict(X_test)
+
+print("Accuracy of SVM with Poly kernel:", metrics.accuracy_score(y_test, y_pred))
+
+# Part 4 of ICP4
+# The SVM automatically uses the RBF kernel when none is provided, so this model is identical to the SVM model above
+
+svc = SVC(kernel='rbf', gamma='auto')
+svc.fit(X_train, y_train)
+y_pred = svc.predict(X_test)
+
+print("Accuracy of SVM with RBF kernel:", metrics.accuracy_score(y_test, y_pred))
+
